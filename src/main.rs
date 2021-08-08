80e5f4e9ce7580d277c21a995a93bc279c23dea0
Mart <themcman@gmail.com>
2021-08-07 19:29:39 -0500

    added panic msgs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
     snake_bevy::run();
 }
